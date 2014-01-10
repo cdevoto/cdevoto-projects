c6406359c1d830d89bd5541bbcb918937d5256c7
bfhcpd0 <bfhcpd0@dtw043033macn03.wowway.com>
2014-01-10 10:55:15 -0500

    Updated RecurrentTestScheduler project to reflect the changes to the Engine project.

--- RecurrentTestScheduler/src/com/compuware/ruxit/synthetic/scheduler/recur/engine/RecurrentTestSchedulingEngine.java
+++ RecurrentTestScheduler/src/com/compuware/ruxit/synthetic/scheduler/recur/engine/RecurrentTestSchedulingEngine.java
@@ -1,11 +1,17 @@
 package com.compuware.ruxit.synthetic.scheduler.recur.engine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.compuware.ruxit.synthetic.di.AppContextProvider;
 import com.compuware.ruxit.synthetic.engine.Engine;
 
 public class RecurrentTestSchedulingEngine extends Engine {
 
 	private static final String APP_NAME = "Recurrent Test Scheduler";
+	
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(RecurrentTestSchedulingEngine.class);
 
 	@Override
 	public String getApplicationName() {
